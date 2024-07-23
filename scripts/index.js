87fc193a58c3b5066208f7816e3119a4f83925fa
Anna Leshcheva <dediaevanna@gmail.com>
2024-07-22 19:37:58 -0500

    Project done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(cardLink, cardName, cardRemover) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardLink;
+  cardElement.querySelector(".card__title").textContent = cardName;
 
-// @todo: Функция создания карточки
+  const button = cardElement.querySelector(".card__delete-button");
+  button.addEventListener("click", function () {
+    cardRemover(cardElement);
+  });
 
-// @todo: Функция удаления карточки
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function removeCard(cardElement) {
+  cardElement.remove();
+}
+
+initialCards.forEach(function (card) {
+  const cardElement = createCard(card.link, card.name, removeCard);
+  placesList.append(cardElement);
+});
